5ea3af009dcaa6ffcb1b4535185fe0511263a168
erayayaz <eray19ayaz@gmail.com>
2023-08-30 20:29:13 +0300

    react 18 index.tsc return to like 17

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import Portfolio from './components/portfolio';
 import {Provider} from "react-redux";
 import {initStore} from "./redux/initStore";
 
 const store = initStore();
 
-const root = ReactDOM.createRoot (
-    document.getElementById('root') as HTMLElement
-)
-
-root.render(
-    <Provider store={store}>
-        <Portfolio />
-    </Provider>,
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <Portfolio />
+        </Provider>,
+    </React.StrictMode>,
+    document.getElementById('root')
 );
